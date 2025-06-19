dc7f261af44f8a5394b56e6a27926271cbdf5f97
Chinnuh3 <109194528+Chinnuh3@users.noreply.github.com>
2025-06-19 19:15:52 +0530

    Update Logger.java

--- Logger.java
+++ Logger.java
@@ -1,19 +1,1 @@
-package designpattern;
 
-public class Logger {
-	private static Logger instance;
-	private Logger()
-	{
-		System.out.println("Logger is created");
-	}
-	public static Logger getInstance() {
-        if (instance == null) {
-            instance = new Logger();
-        }
-        return instance;
-    }
-	public void log(String message) {
-        System.out.println("logger " + message);
-    }
-
-}
